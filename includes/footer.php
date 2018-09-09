c301953903d31dc11958756e2e0589bf55f72e1e
Obydullah Shishir <shishir.srdr16@gmail.com>
2018-09-09 17:06:55 +0600

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,26 @@
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="text-center">Copyright &copy; 2018 - Dev-Hub 2.0</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
